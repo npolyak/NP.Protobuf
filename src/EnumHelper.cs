7b8adc159ed88a3d4d159c7d88a63699773d4554
npolyak <nick.polyak@verizon.net>
2023-01-02 18:51:53 -0500

    refactored.

---
+++ src/EnumHelper.cs
@@ -0,0 +1,12 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using NP.Utilities;
+
+namespace NP.Protobuf;
+
+public static class EnumHelper
+{
+    public static EnumValue ToEnumVal(this System.Enum e)
+    {
+        return new EnumValue { Name = nameof(e), Number = e.ToInt() };
+    }
+}
