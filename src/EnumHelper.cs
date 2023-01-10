3ec3a6c3bfb79c34938eeae69c0e9898aa928d1f
npolyak <nick.polyak@verizon.net>
2023-01-10 13:36:59 -0500

    moved messages to RelayService for simplification

--- src/EnumHelper.cs
+++ src/EnumHelper.cs
@@ -9,13 +9,4 @@ public static class EnumHelper
     {
         return new EnumValue { Name = System.Enum.GetName(e.GetType(), e), Number = e.ToInt() };
     }
-
-    public static ShortMsg ToShortMsg(this System.Enum topic) 
-    {
-        return new ShortMsg
-        {
-            MsgSentTime = Timestamp.FromDateTime(DateTime.UtcNow),
-            Topic = topic.ToEnumVal()
-        };
-    }
 }
